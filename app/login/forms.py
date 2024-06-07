1289bbd2594ebe980d346aca5200fbfed3c741ef
Peter Sido <63193494+PeterSIdo@users.noreply.github.com>
2024-06-07 21:03:48 +0100

    eveluate user levels

--- app/login/forms.py
+++ app/login/forms.py
@@ -4,7 +4,12 @@ from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    user_mode = SelectField('User Mode', choices=[('a', 'Admin'), ('c', 'Carer')], validators=[DataRequired()])
+    user_mode = SelectField('User Mode', choices=[
+        ('c', 'Carer'), 
+        ('m', 'Manager'), 
+        ('f', 'Family'), 
+        ('a', 'Admin')
+    ], validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
